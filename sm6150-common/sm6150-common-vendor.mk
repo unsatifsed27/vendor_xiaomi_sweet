587ca18c26379a61ef8f7e2a3426c2071141ec69
basamaryan <basam.aryan@gmail.com>
2022-08-05 16:30:06 +0400

    sm6150-common: Update blobs from sweet V13.0.12.0.SKFMIXM
    Change-Id: I0c8c8d82779c433fd76004fd0e81e9156e5f16b4

--- sm6150-common/sm6150-common-vendor.mk
+++ sm6150-common/sm6150-common-vendor.mk
@@ -126,6 +126,8 @@ PRODUCT_PACKAGES += \
     libcdsp_default_listener \
     libdiag \
     libdsutils \
+    libfastcvdsp_stub \
+    libfastcvopt \
     libgpudataproducer \
     libgsl \
     libidl \
@@ -140,6 +142,12 @@ PRODUCT_PACKAGES += \
     libqmi_csi \
     libqmi_encdec \
     libqmiservices \
+    libscveCommon \
+    libscveCommon_stub \
+    libscveObjectSegmentation \
+    libscveObjectSegmentation_stub \
+    libscveObjectTracker \
+    libscveObjectTracker_stub \
     libvpphvx \
     libvpplibrary \
     libdirac \
@@ -327,6 +335,7 @@ PRODUCT_PACKAGES += \
     librpmb \
     libsdsprpc \
     libsoc_helper \
+    libsoc_helper_jni \
     libssd \
     libsystem_health_mon \
     libtime_genoff \
@@ -404,6 +413,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.radio.uim_remote_client@1.1 \
     vendor.qti.hardware.radio.uim_remote_client@1.2 \
     vendor.qti.hardware.radio.uim_remote_server@1.0 \
+    vendor.qti.hardware.scve.objecttracker@1.0 \
+    vendor.qti.hardware.scve.panorama@1.0 \
     vendor.qti.hardware.soter@1.0 \
     vendor.qti.ims.callcapability@1.0 \
     vendor.qti.ims.callinfo@1.0 \
